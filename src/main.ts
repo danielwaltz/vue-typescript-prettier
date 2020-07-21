d65953a37fb93f7ea57d51e1d8761035045af5f3
Daniel Waltz <danielbwaltz@gmail.com>
2020-07-21 18:12:50 -0400

    Update dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import App from '@/App.vue';
 Vue.config.productionTip = false;
 
 new Vue({
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
